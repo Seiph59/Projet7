25c7b689c2b418ad40456ea75ca46381f21c4cac
Damien Galasso <damien.galasso@outlook.fr>
2019-04-07 15:52:45 +0200

    remove import config

--- grandpy/appli/googlemaps.py
+++ grandpy/appli/googlemaps.py
@@ -2,7 +2,7 @@
 File used to request to Google's API
 """
 import requests
-from config import API_GOOGLE_MAP_KEY
+# from config import API_GOOGLE_MAP_KEY
 
 GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
